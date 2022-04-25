e032dcbd9ec4b1429d80782bb06c07e9e015f3f9
Yuki326 <s20a1035@gmail.com>
2022-04-25 16:58:36 +0900

    表示に作成者名を追加

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import JankenGame from "./JankenGame";
 function App() {
   return (
     <div className="App">
+      <h1>作成者：坂井 優紀</h1>
       <GuessNumberGame MAX={1000} />
       <JankenGame />
     </div>
